8e714f5ba136289586e4114b1e4f908796a60ae1
LukaszHolowacz <lukasz.holowacz5@gmail.com>
2022-10-18 10:00:18 +0200

    Działanie pierwszego buttona (doklejanie tekstu), proba wywalenia białego paska, który pokazuje się w emulatorze

--- app/src/main/java/com/example/zadnaocene18102022/MainActivity.kt
+++ app/src/main/java/com/example/zadnaocene18102022/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.zadnaocene18102022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wynik = findViewById<TextView>(R.id.wynik)
+        val butdokleja = findViewById<Button>(R.id.doklejatext)
+        val butreset = findViewById<Button>(R.id.mojeimie)
+        val podanytext = findViewById<EditText>(R.id.editTextTextPersonName)
+
+        butdokleja.setOnClickListener {
+            wynik.text = wynik.text.toString() + podanytext.text.toString()
+        }
     }
 }
